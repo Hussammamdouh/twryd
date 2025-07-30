aa8e7b317e82f68a42eaf4b79ebb4be087e05ca9
Hussammamdouh <125016995+Hussammamdouh@users.noreply.github.com>
2025-07-30 11:13:25 +0300

    Prepare for GitHub Pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/twryd/', // 👈 this tells Vite where your project will be hosted
   plugins: [react()],
 })
