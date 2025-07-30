d730883eeb2d5127945ec2d7a24ff39b01a8404b
Hussammamdouh <125016995+Hussammamdouh@users.noreply.github.com>
2025-07-30 11:38:17 +0300

    Fixed bug in the Deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/twryd/', // 👈 this tells Vite where your project will be hosted
+  base: '/twryd/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        notFound: '404.html',
-      },
-    },
-  },
 })
